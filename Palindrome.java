089297c4c58a9443e4e00ef841fa8392813e739b
nikitaajay <145547016+nikitaajay@users.noreply.github.com>
2023-10-03 14:59:21 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,44 @@
+/**************************************************************************************
+ * File:Palindrome.java
+ * Description:To find whether a word is palindrome or not
+ * Author:Nikita Ajay
+ * Version:1.0
+ * Date:26/09/23
+ * ************************************************************************************/
+package javaLab;
+import java.util.Scanner;
+public class Palindrome {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the word to be checked:");
+		String input=sc.next();
+	boolean word =isPalindrome(input);
+	if(word)
+	{
+		System.out.println("The given word is palindrome:");
+	}
+	else
+	{
+		System.out.println("The given word is not palindrome:");
+	}
+	}
+	/**************************************************************************************
+	 * To check whether a given word is palindrome or not we use a function called isPalindrome
+	 */
+	static boolean isPalindrome(String input) {
+		char a[]=input.toCharArray();
+		int len=input.length();
+		for(int i=0;i<=len/2;i++)
+			if(a[i]!=a[len-1-i])
+			{
+				return false;
+			}
+	
+	return true;
+}
+
+			
+		
+		
+		
+}
